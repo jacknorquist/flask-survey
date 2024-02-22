f9534cb2f399f99aff2d1ed17bb3fc66cf86bedd
Jack Norquist <jack.norquist@outlook.com>
2024-02-22 14:05:41 -0600

    Init2

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses =[]
+
+@app.get("/")
+def serve_home_page():
+    return render_template('survey_start.html')
+
+@app.get('/questions/<question_name>')
+def serve_questions():
+    return render_template('question.html')
+
