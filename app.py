1aef91b941cff3c351eefaf1422007fe1e65bcfb
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-02-22 18:00:14 -0800

    Part 5 is done

--- app.py
+++ app.py
@@ -12,36 +12,44 @@ responses =[]
 
 @app.get("/")
 def serve_home_page():
-    return render_template('survey_start.html')
+    """Clears the responses list and starts the survey"""
+
+    return render_template('survey_start.html', survey = survey)
 
 @app.post('/begin')
-def serve_questions():
-
-
+def start_survey():
+    """Redirects to the first question"""
+    responses.clear()
     return redirect('/questions/0')
 
-@app.get('/questions/<new_question>')
-def first_question(new_question):
+@app.get('/questions/<int:question_id>')
+def first_question(question_id):
+    """Shows the question and gives choices."""
 
-    question = survey.questions[int(new_question)]
-
+    question = survey.questions[int(question_id)]
 
     return render_template('question.html', question = question)
 
 @app.post('/answer')
-def get_response():
-    responses.append(request.form.get('answer'))
+def handle_answers():
+    """Appends answers to responses list and redirects to new question
+    if available. If not it redirects to the thank you page"""
+    selected_choice = request.form['answer']
+    responses.append(selected_choice)
     if len(responses) >= len(survey.questions):
         return redirect('/thankyou')
-    else:
-        question = str(len(responses))
 
+    question_number = len(responses)
 
-        return redirect(f'/questions/{question}')
+    return redirect(f'/questions/{question_number}')
 
 @app.get('/thankyou')
 def get_thank_you_message():
+    """Returns a thank you message with questions and answers"""
     survey_questions = survey.questions
     active_responses = responses
-    responses = []
-    return render_template('completion.html', survey_questions= survey_questions, active_responses=active_responses)
+    return render_template(
+        'completion.html',
+        survey_questions= survey_questions,
+        active_responses=active_responses
+        )
