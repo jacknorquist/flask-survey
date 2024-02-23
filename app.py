36c82d5955258072e322141788a94c2b34244754
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-02-22 19:15:41 -0800

    Completed the exercise

--- app.py
+++ app.py
@@ -8,23 +8,27 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses =[]
+
 
 @app.get("/")
 def serve_home_page():
-    """Clears the responses list and starts the survey"""
-
+    """Clears the session[“session["responses"]”] list and starts the survey"""
+    session["responses"] =[]
     return render_template('survey_start.html', survey = survey)
 
 @app.post('/begin')
 def start_survey():
     """Redirects to the first question"""
-    responses.clear()
+    session["responses"].clear()
     return redirect('/questions/0')
 
 @app.get('/questions/<int:question_id>')
 def first_question(question_id):
     """Shows the question and gives choices."""
+
+    if question_id > len(session["responses"]):
+        flash("Please answer the previous questions.")
+        return redirect(f'/questions/{len(session["responses"])}')
 
     question = survey.questions[int(question_id)]
 
@@ -32,14 +36,16 @@ def first_question(question_id):
 
 @app.post('/answer')
 def handle_answers():
-    """Appends answers to responses list and redirects to new question
+    """Appends answers to session[“responses”] list and redirects to new question
     if available. If not it redirects to the thank you page"""
     selected_choice = request.form['answer']
-    responses.append(selected_choice)
-    if len(responses) >= len(survey.questions):
+    selected_choices = session["responses"]
+    selected_choices.append(selected_choice)
+    session["choices"] = selected_choices
+    if len(session["responses"]) >= len(survey.questions):
         return redirect('/thankyou')
 
-    question_number = len(responses)
+    question_number = len(session["responses"])
 
     return redirect(f'/questions/{question_number}')
 
@@ -47,7 +53,7 @@ def handle_answers():
 def get_thank_you_message():
     """Returns a thank you message with questions and answers"""
     survey_questions = survey.questions
-    active_responses = responses
+    active_responses = session["responses"]
     return render_template(
         'completion.html',
         survey_questions= survey_questions,
